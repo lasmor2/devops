2860d7fcd5356d09d44787f228109991601bcb55
lasmor <llincohse@gmail.com>
2025-09-19 01:47:37 +0100

    add Eslint and Prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express"
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send("hello from devops")
-})
+app.get('/', (req, res) => {
+  res.status(200).send('hello from devops');
+});
 
-export default app
+export default app;
